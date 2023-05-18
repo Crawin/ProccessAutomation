8731ed1cbb3b17ac95aeb9af26e7e724e05bfc78
crawin_nexon <31588>
2023-05-18 17:40:02 +0900

    G18 부터는 키워드 삭제하는 방식이 변경되어 6.py를 수정하여 사용

--- python/pyauto/6.py
+++ python/pyauto/6.py
@@ -7,8 +7,8 @@ original = original.split('\n')
 
 result = ''
 for i in range(0,len(original)):
-    if 'me.RemoveKeyword(`' in original[i]:
-        temp = original[i].replace('me.RemoveKeyword(`','').replace('`);','').replace('\t','')
+    if 'player.RemoveKeyword(`' in original[i]:
+        temp = original[i].replace('player.RemoveKeyword(`','').replace('`);','').replace('\t','')
         result += temp + '\n'
 copy(result)
 print(result)
